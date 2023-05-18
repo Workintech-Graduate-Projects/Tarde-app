dd5b69e87847442c434edb3f6ab671604a8660fe
BuseMutluoglu <busemutluoglu@gmail.com>
2023-05-18 11:47:43 +0300

    Dummy data oluşturuldu

---
+++ pages/dummy-data.js
@@ -0,0 +1,59 @@
+const dummyData = [
+  {
+    id: 1,
+    sehir: "Gaziantep",
+    personelAdi: ["Sümeyra Al", "Canan Tütünen"],
+    telefonNumaralari: ["0532 330 16 33", "0539 774 35 75"],
+    ulasilanKisi: 1000,
+    aracSayisi: 1,
+    merkezler: [
+      "Nurdağı 1 Konteyner Kenti - Görüşme konteynerı",
+      "Mobil Ekip (Nurdağı B-1 C1, TOKİ Konteyner Eğitim Kampüsü, Ahbap Okulları, Nurdağı 2, Fatih Mah. Yeni Mah. Kurudere Mah. )",
+    ],
+    isbirlikciKurumlar: [
+      "WHR",
+      "AÇEV",
+      "ASHB",
+      "Sağlık Bakanlığı",
+      "GASMEK",
+      "Halk Eğitim Merkezi",
+      "TOG",
+      "Gaziantep Yardım Vakfı",
+      "Empati Derneği",
+      "KAÇUV",
+      "Diyanet",
+      "Yöret-Roman Diyalog Ağı - Sıfır Ayrımcılık Derneği( işbirliği için iletişime geçildi).",
+    ],
+  },
+  {
+    id: 2,
+    sehir: "Adıyaman",
+    personelAdi: ["Gizem Yılmaz"],
+    telefonNumaralari: [""],
+    ulasilanKisi: 236,
+    aracSayisi: 0,
+    merkezler: [
+      "Mersin Büyükşehir Belediyesi'nin Konteyner Kenti'nde K12 çalışma ve yaşam ofisi bulunmakta. Alt yapı çalışmalarının bitmesi bekleniyor.",
+    ],
+    isbirlikciKurumlar: [
+      "Aile ve Sosyal Hizmetler Bakanlığı",
+      "AÇEV",
+      "UNICEF",
+    ],
+  },
+  {
+    id: 3,
+    sehir: "Kahramanmaraş",
+    personelAdi: ["Yarencan Çingir", "Ayça Dağılan", "Gözde Murt"],
+    telefonNumaralari: ["0549 553 21 00"],
+    ulasilanKisi: 24,
+    aracSayisi: 0,
+    merkezler: [
+      "İzmit & Nilüfer Belediyesi Çadırkent - Kahramanmaraş, Onikişubat",
+      "Doğa Koleji - Kahramanmaraş, Onikişubat",
+    ],
+    isbirlikciKurumlar: ["İzmit Belediyesi"],
+  },
+];
+
+export default dummyData;
