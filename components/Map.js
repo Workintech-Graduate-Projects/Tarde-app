4a9454cd39160e460e1d1a145d05e4edd24a9f00
Ömür <omurcanuslu@outlook.com>
2023-05-21 18:22:03 +0300

    redux setup

--- components/Map.js
+++ components/Map.js
@@ -104,6 +104,10 @@ function Maps() {
         {
           type: "Feature",
           properties: {
+            name: dummyData[6].sehir,
+            ulasilan: dummyData[6].ulasilanKisi,
+            telefon: dummyData[6].telefonNumaralari[0],
+            yetkili: dummyData[6].personelAdi[0],
 
           },
           geometry: {
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
 import "@/styles/globals.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { NextUIProvider } from "@nextui-org/react";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { mainReducer } from "@/redux/actions";
+import thunk from "redux-thunk";
 
 export default function App({ Component, pageProps }) {
+  const store= createStore(mainReducer, applyMiddleware(thunk))
   return (
     <>
+    <Provider store={store}>
       <NextUIProvider>
         <Component {...pageProps} />
       </NextUIProvider>
+    </Provider>
     </>
   );
 }
